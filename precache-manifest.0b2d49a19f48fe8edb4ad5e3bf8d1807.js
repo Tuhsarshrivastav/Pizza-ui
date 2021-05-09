c55580f8870da465049f60bee0ca874c0f4e011a
Tusharshrivastav <ts3657@gmail.com>
2021-05-09 06:21:13 +0530

    Updates

---
+++ precache-manifest.0b2d49a19f48fe8edb4ad5e3bf8d1807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27af520903a5032f10ec3ccd1d71c33c",
+    "url": "/Pizza-ui/index.html"
+  },
+  {
+    "revision": "d48aca659926e7789ecc",
+    "url": "/Pizza-ui/static/css/main.ceaeed2b.chunk.css"
+  },
+  {
+    "revision": "82f39457559270b99e80",
+    "url": "/Pizza-ui/static/js/2.38dfb4cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pizza-ui/static/js/2.38dfb4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48aca659926e7789ecc",
+    "url": "/Pizza-ui/static/js/main.1d844e7b.chunk.js"
+  },
+  {
+    "revision": "25dfe24143afd5775d04",
+    "url": "/Pizza-ui/static/js/runtime-main.d295841e.js"
+  }
+]);
